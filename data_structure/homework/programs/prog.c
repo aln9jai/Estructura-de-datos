836a2b2050497d2af894f2ee29e8d0f1aaaea258
aln9jai <145497921+aln9jai@users.noreply.github.com>
2025-09-02 18:46:06 -0600

    Actualizar prog.c

--- data_structure/homework/programs/prog.c
+++ data_structure/homework/programs/prog.c
@@ -1,1 +1,15 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	int i;
+	char *num = argv[1];
+	
+	fprintf(stdout, "arg[1] = %s \n", argv[1]);
+	
+	for (i = 0; 1 < strlen(argv[1]); i++)
+	    fprintf(stdout, "num[%d] = %c \n", i, num[i]);
+	    
+	return 0;
+}
